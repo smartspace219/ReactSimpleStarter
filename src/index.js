8e36dfa2d06baab2c59ee96792b4136c477a0201
smartspace <insmartspace@gmail.com>
2024-02-07 16:20:38 -0800

    commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './example/useReducer1';
+import App from './example/useReducer2';
 
 import reportWebVitals from './reportWebVitals';
 
